bbdd23fed9a314cadf3b7e2b7f809886407f2efe
Ashish Kumar <ashish2108@gmail.com>
2020-08-05 02:55:58 +0530

    more changes to status page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
 def main
 require 'socket'
-render plain: "Host: #{Socket.gethostname} \nPrivate IP: #{Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address)}"
+render plain: "Host: #{Socket.gethostname} \nPrivate IP: #{Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address)}\nGit version: #{`git show --pretty=%H___%ai -q`}"
 #render plain: debug(request) # .inspect
 end
 
