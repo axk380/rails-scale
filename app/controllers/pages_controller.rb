cd467f8400ed1110cd810f8476cdbce16c70f927
Ashish Kumar <ashish2108@gmail.com>
2020-08-07 03:13:50 +0530

    elb test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
 def main
 require 'socket'
-idenfier_str = "cap update 3: deploy should not have asked any password"
+idenfier_str = "cap update 4: this should appear from load balancer"
 render plain: "Host: #{Socket.gethostname} \nPrivate IP: #{Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address)}\nGit version: #{`git show --pretty=%H___%ai -q`} Identifier: #{idenfier_str} \n Last Name: #{Name.last[:name]}"
 #render plain: debug(request) # .inspect
 end
