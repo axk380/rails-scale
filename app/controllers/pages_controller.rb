e2758e248d42c275151e2bb04ab2c2c271c75ccd
Ashish Kumar <ashish2108@gmail.com>
2020-08-07 02:40:42 +0530

    cap1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
 def main
 require 'socket'
-idenfier_str = "AAA"
+idenfier_str = "cap update 1"
 render plain: "Host: #{Socket.gethostname} \nPrivate IP: #{Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address)}\nGit version: #{`git show --pretty=%H___%ai -q`} Identifier: #{idenfier_str} \n Last Name: #{Name.last[:name]}"
 #render plain: debug(request) # .inspect
 end
